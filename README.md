e5e156016111ab2748b770e75bfe1a0d8b6a3e97
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2025-02-14 18:07:21 -0600

    beginning with a player

---
+++ README.md
@@ -0,0 +1,2 @@
+## Godot Low-Level Server for Optimization 
+Testing the use of low-level server functions to reduce the load of huge numbers of images
---
+++ player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@onready var sprite_2d = $Sprite2D
+@export var speed: int = 500
+
+
+func _process(_delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	# face sprite in correct direction
+	if (direction.x < -0.1):
+		sprite_2d.set_flip_h( true )
+	elif (direction.x > 0.1):
+		sprite_2d.set_flip_h( false )
+	
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
